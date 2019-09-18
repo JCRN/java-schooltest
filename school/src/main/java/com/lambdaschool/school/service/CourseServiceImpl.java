71281c3057cb3edd8bc6278ab5776ddac94d5e24
jcrn <clemons.jason@gmail.com>
2019-09-18 15:48:57 -0700

    fixed SeedData file

--- school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.school.service;
 
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.repository.CourseRepository;
 import com.lambdaschool.school.view.CountStudentsInCourses;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,26 @@ public class CourseServiceImpl implements CourseService
     }
 
     @Override
+    public Course findCourseById(long id) throws EntityNotFoundException {
+        return courserepos.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Long.toString(id)));
+    }
+
+    @Override
     public ArrayList<CountStudentsInCourses> getCountStudentsInCourse()
     {
         return courserepos.getCountStudentsInCourse();
+    }
+
+    @Transactional
+    @Override
+    public Course save(Course course)
+    {
+        Course newCourse = new Course();
+
+        newCourse.setCoursename(course.getCoursename());
+
+        return courserepos.save(newCourse);
     }
 
     @Transactional
